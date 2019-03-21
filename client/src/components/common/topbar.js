4199272fd52d287fae73fea3d2d1fa563cfcc271
adityasrivast <yashsri421@gmail.com>
2019-03-21 16:06:11 +0530

    restructure app

---
+++ client/src/components/common/topbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './topbar.css';
+
+const Topbar = () => {
+  return (
+    <nav>
+      <div>
+        <span className="theme-color">C2C</span>&nbsp;&nbsp;&nbsp;
+        <span style={{color: '#fff'}}>Projects</span>
+      </div>
+    </nav>
+  );
+}
+
+export default Topbar;
