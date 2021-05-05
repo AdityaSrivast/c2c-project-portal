c2af083cb3f004d54eab8d35bafe0be6b95468ac
Aditya Srivastava <yashsri421@gmail.com>
2021-05-05 23:08:30 +0530

    Updates

---
+++ precache-manifest.08437a652e18b8b0533ccb42511040bf.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "a730c1560800bf77017479a0539ff497",
+    "url": "/c2c-project-portal/static/media/bg1.a730c156.svg"
+  },
+  {
+    "revision": "178c33ee3ee2f31bc843",
+    "url": "/c2c-project-portal/static/css/main.33e06b87.chunk.css"
+  },
+  {
+    "revision": "aa73fa9f251f7c37ab9c",
+    "url": "/c2c-project-portal/static/js/runtime~main.cc3bf050.js"
+  },
+  {
+    "revision": "962016706efae5a1abc3",
+    "url": "/c2c-project-portal/static/js/2.c45bb114.chunk.js"
+  },
+  {
+    "revision": "36b95d1079e11cc557e0a868171626d7",
+    "url": "/c2c-project-portal/static/media/CircularStd-Book.36b95d10.woff2"
+  },
+  {
+    "revision": "fdbfc358bc6e87a42b57707567214dc6",
+    "url": "/c2c-project-portal/static/media/CircularStd-Bold.fdbfc358.woff2"
+  },
+  {
+    "revision": "74679a4cc017dca4621d6bcf38381806",
+    "url": "/c2c-project-portal/static/media/CircularStd-Bold.74679a4c.woff"
+  },
+  {
+    "revision": "d356ee6e9377e36166652b0dab169354",
+    "url": "/c2c-project-portal/static/media/CircularStd-Book.d356ee6e.woff"
+  },
+  {
+    "revision": "178c33ee3ee2f31bc843",
+    "url": "/c2c-project-portal/static/js/main.c63c367e.chunk.js"
+  },
+  {
+    "revision": "1d4059cebfac924cb8427eac31c0e223",
+    "url": "/c2c-project-portal/static/media/CircularStd-Book.1d4059ce.eot"
+  },
+  {
+    "revision": "43f768f8fbe2c37b1fb880e517a563d3",
+    "url": "/c2c-project-portal/static/media/CircularStd-Book.43f768f8.ttf"
+  },
+  {
+    "revision": "56f82cac95e52c85498a25a30d9774e9",
+    "url": "/c2c-project-portal/static/media/CircularStd-Bold.56f82cac.eot"
+  },
+  {
+    "revision": "df13ba46cad8f788b03e29ab535c8f7d",
+    "url": "/c2c-project-portal/static/media/CircularStd-Bold.df13ba46.ttf"
+  },
+  {
+    "revision": "f053c365f9f1b7cfd3c11fa525eb58b7",
+    "url": "/c2c-project-portal/static/media/CircularStd-Book.f053c365.svg"
+  },
+  {
+    "revision": "ec482ba4f1945bb0a3371af7972c1597",
+    "url": "/c2c-project-portal/static/media/CircularStd-Bold.ec482ba4.svg"
+  },
+  {
+    "revision": "0b6686f92e5644b3d6fa1429810ddb42",
+    "url": "/c2c-project-portal/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3dcd8fd73208f09f3a29c56e196b39f1.js"
+  "/c2c-project-portal/precache-manifest.08437a652e18b8b0533ccb42511040bf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/c2c-project-portal/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
